a4319ff6cc6eb4867a7621ad3f7105abc437ecb9
hayderjebur <49797771+hayderjebur@users.noreply.github.com>
2024-05-03 12:06:24 -0700

    Add Browser Router

--- server/server.js
+++ server/server.js
@@ -61,13 +61,14 @@ app.use('/api/users', userRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const parentDir = path.join(__dirname, '..');
+console.log('server outside if fired', __dirname);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(parentDir, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
-    console.log('server get * fired', parentDir);
-    res.sendFile(path.resolve(parentDir, 'client', 'build', 'index.html'));
+    console.log('server inside if fired', __dirname);
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
